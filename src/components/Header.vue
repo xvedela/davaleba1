563616167c37903c4eb255cb549c2051fd2dd861
xvedela <dato.xvedelidze.888@gmail.com>
2024-01-09 17:22:21 +0400

    first commit

---
+++ src/components/Header.vue
@@ -0,0 +1,12 @@
+<script setup>
+</script>
+
+<template>
+<div class="max-w-7xl mx-auto hidden md:flex justify-end">
+  <a href="" class="py-5 px-2 border-b border-black">მთავარი</a>
+  <a href="" class="py-5 px-2 border-b border-black">გალერეა</a>
+  <a href="" class="py-5 px-2 border-b border-black">მხატვრის შესახებ</a>
+  <a href="" class="py-5 px-2 border-b border-black">კონტაქტი</a>
+  <a href="" class="py-5 px-2">შესვლა</a>
+</div>
+</template>
---
+++ src/components/Home.vue
@@ -0,0 +1,151 @@
+<script setup>
+import mainLayout from '@/layouts/mainLayout.vue';
+</script>
+
+<template>
+  <mainLayout>
+    <div class="flex">
+      <div class="lg:w-90">
+        <img src="../images/image1.png">
+      </div>
+      <div class="ml-60 text-5xl pt-40">
+        <a class="font-extrabold mb-20 block">ლეონარდოს ფუნჯი</a>
+        <a class="mb-4 block text-lg ml-10">ხელოვნება არასოდეს მთავრდება</a>
+      </div>
+    </div>
+
+    <!-- Div 2 start -->
+    <div class="flex flex-col items-center">
+      <h1 class="text-5xl font-bold mt-8 mb-4">გალერეა</h1>
+
+
+      <div class="flex justify-center space-x-4">
+        <div class="border rounded p-4">
+          <img src="../images/arow1.png" alt="Arrow 1" class="max-w-full h-auto">
+          <div class="mt-4">
+            <h3 class="text-lg font-bold">Product Name</h3>
+            <p class="text-gray-500">Price: $19.99</p>
+            <button class="bg-blue-500 text-white px-4 py-2 mt-2 rounded">Buy Now</button>
+          </div>
+        </div>
+
+
+
+
+        <div class="flex justify-center space-x-4">
+          <div class="border rounded p-4">
+            <img src="../images/arow2.png" alt="Arrow 1" class="max-w-full h-auto">
+            <div class="mt-4">
+              <h3 class="text-lg font-bold">Product Name</h3>
+              <p class="text-gray-500">Price: $19.99</p>
+              <button class="bg-blue-500 text-white px-4 py-2 mt-2 rounded">Buy Now</button>
+            </div>
+          </div>
+        </div>
+
+
+        <div class="flex justify-center space-x-4">
+          <div class="border rounded p-4">
+            <img src="../images/arow3.png" alt="Arrow 1" class="max-w-full h-auto">
+            <div class="mt-4">
+              <h3 class="text-lg font-bold">Product Name</h3>
+              <p class="text-gray-500">Price: $19.99</p>
+              <button class="bg-blue-500 text-white px-4 py-2 mt-2 rounded">Buy Now</button>
+            </div>
+          </div>
+        </div>
+
+        <div class="flex justify-center space-x-4">
+          <div class="border rounded p-4">
+            <img src="../images/arow4.png" alt="Arrow 1" class="max-w-full h-auto">
+            <div class="mt-4">
+              <h3 class="text-lg font-bold">Product Name</h3>
+              <p class="text-gray-500">Price: $19.99</p>
+              <button class="bg-blue-500 text-white px-4 py-2 mt-2 rounded">Buy Now</button>
+            </div>
+          </div>
+        </div>
+
+        <div class="flex justify-center space-x-4">
+          <div class="border rounded p-4">
+            <img src="../images/arow5.png" alt="Arrow 1" class="max-w-full h-auto">
+            <div class="mt-4">
+              <h3 class="text-lg font-bold">Product Name</h3>
+              <p class="text-gray-500">Price: $19.99</p>
+              <button class="bg-blue-500 text-white px-4 py-2 mt-2 rounded">Buy Now</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- Div end -->
+
+    <!-- Div start -->
+    <div class="pt-40">
+      <div class="flex">
+        <div class="lg:w-90">
+          <h1 class="pl-[600px] text-4xl font-extrabold mb-20 block ">მხატვრის შესახებ</h1>
+          <div class="flex">
+            <div class="flex-shrink-0">
+              <img src="../images/image1.png" class="object-cover" alt="Image">
+            </div>
+            <p class="mb-4 pt-20 pl-[100px] block text-lg ml-10">
+              ლეონარდო და ვინჩმა, რომელიც დაიბადა ვინჩიში, იტალიაში, 1452 წელს, განასახიერა რენესანსული სულისკვეთება,
+              როგორც პოლიმათი. მისმა მხატვრულმა ბრწყინვალებამ შექმნა შედევრები, როგორიცაა "მონა ლიზა" და "ბოლო ვახშამი",
+              რომლებიც აჩვენებენ სინათლის, ჩრდილის და პერსპექტივის უბადლო გაგებას. ანატომიაში ჩაღრმავებამ, მისმა დისექციამ
+              წარმოადგინა რთული ჩანახატები, რომლებიც ავლენს ადამიანის ფორმის ღრმა კვლევას.
+
+              ლეონარდოს საინჟინრო გამოგონება იყო შორსმიმავალი, ითვალისწინებდა საფრენ აპარატებს, სამხედრო ინოვაციებს და
+              არქიტექტურულ დიზაინს. მისი რვეულები, იდეებისა და დაკვირვებების საგანძური, მოიცავდა ანატომიას, ბოტანიკას,
+              გეოლოგიასა და ჰიდრავლიკას. მისი ხედვითი კონცეფციები ხშირად აჭარბებდა თავისი დროის ტექნოლოგიურ
+              შესაძლებლობებს.
+
+              მისი მხატვრული და მეცნიერული წვლილის გარდა, ლეონარდოს მახვილი დაკვირვებები გავრცელდა ბუნებრივ სამყაროზეც და
+              დოკუმენტურად აღწერდა ფენომენებს წყლის ნაკადიდან ფრინველების ფრენამდე. მისი მემკვიდრეობა სცილდება
+              დისციპლინებს, რაც სიმბოლოა ხელოვნებისა და მეცნიერების ურთიერთკავშირში. ლეონარდო და ვინჩის დაუოკებელი
+              ცნობისმოყვარეობა და პიონერული სული კვლავ შთააგონებს თაობებს, აძლიერებს მის სტატუსს, როგორც ინტერდისციპლინური
+              გენიოსის მაგალითს. იგი გარდაიცვალა 1519 წელს და დატოვა ღრმა და მუდმივი გავლენა ადამიანის შემოქმედებასა და
+              კვლევაზე.
+            </p>
+          </div>
+        </div>
+      </div>
+      <!-- Div end -->
+
+
+      <div class="flex p-4 bg-black text-white">
+        <div class="w-1/2 p-4">
+          <h2 class="text-2xl font-bold mb-4">კონტაქტი</h2>
+          <div class="mb-4">
+            <label for="name" class="block text-white">თბილისი, ლეონარდო და ვინჩის ქუჩა N15</label>
+          </div>
+          <div class="mb-4">
+            <label for="email" class="block">
+              მობილური: <span class="text-blue-500">(+995) 551 10 11 12</span>
+            </label>
+          </div>
+          <div class="mb-4">
+            <label for="email" class="block">
+              მეილი: <span class="text-blue-500">ლეონარდოს_ფუნჯი@gmail.com</span>
+            </label>
+          </div>
+        </div>
+        <div class="w-1/2 p-4"> <!-- Updated background and text color for the second half -->
+          <h2 class="text-2xl font-bold mb-4"></h2>
+          <div class="mb-4">
+            <input placeholder="მეილი" type="text" id="subject" name="subject" class="w-full border rounded p-2">
+          </div>
+          <div class="mb-4">
+            <input placeholder="სათაური" type="text" id="subject" name="subject" class="w-full border rounded p-2">
+          </div>
+          <div class="mb-4">
+            <textarea placeholder="შეტყობინება" id="message" name="message" rows="4"
+              class="w-full border rounded p-2"></textarea>
+          </div>
+          <button class="bg-blue-500 text-white py-2 px-4 rounded">გაგზავნა</button>
+        </div>
+      </div>
+    </div>
+  </mainLayout>
+</template>
+
